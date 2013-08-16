c5a0e9b49f7023323fb6c0784f0b7017a5726edd
jdherman <jdherman8@gmail.com>
2013-08-16 10:00:23 -0400

    testing..

--- ADM_ML/sidebar.php
+++ ADM_ML/sidebar.php
@@ -6,7 +6,7 @@ $sidebar_groups = array("Forcing" =>
                                                   "6" => array("value" => "Tmax", "title" => $_("Maximum Temperature (C)")),
                                                   "7" => array("value" => "Temp", "title" => $_("Minimum Temperature (C)")),
                                                   "8" => array("value" => "Wind", "title" => $_("Wind (m/s)"))),
-                              "title" => $_("Meteorology")
+                              "title" => $_("Meteorology"),
                               "infobox_number" => 1),
 
                         "Model" =>
@@ -14,12 +14,12 @@ $sidebar_groups = array("Forcing" =>
                                                   "10" => array("value" => "Sm_1", "title" => $_("Soil Moisture(%) - Layer 1")),
                                                   "11" => array("value" => "Sm_2", "title" => $_("Soil Moisture(%) - Layer 2")),
                                                   "14" => array("value" => "runoff", "title" => $_("Surface Runoff (mm/day)"))),
-                              "title" => $_("Hydrologic Variables")
+                              "title" => $_("Hydrologic Variables"),
                               "infobox_number" => 2),
 
                         "Drought" =>
                         array("products" => array("15" => array("value" => "smqall", "title" => $_("Drought Index"))),
-                              "title" => $_("Drought Products")
+                              "title" => $_("Drought Products"),
                               "infobox_number" => 3)
                         );
 
@@ -76,7 +76,15 @@ foreach($sidebar_groups as $key => $value) {
     <?php echo $sidebar_groups[$key]["title"]?> 
     <img id="question_mark" src="icons/question_icon.png" onmouseover=<?php echo "Info_Box_Call(".$sidebar_groups[$key]["infobox_number"].")"?> onmouseout=<?php echo "Info_Box_Call(".$sidebar_groups[$key]["infobox_number"].")"?>>
   </h1>
-  
+  <div id=<?php echo $key."_div"?> style="visibility:visible;">
+    <?php 
+    foreach($value["products"] as $productID => $productAttr) {
+    ?>
+      <input id=<?php echo "overlayImageSelect_".$productID?> type="radio" name="group1" value=<?php echo $productAttr["value"]?> onclick=animate_overlay(<?php echo $productID?>)> <?php echo $productAttr("title")?> <br/>
+    <?php
+    }
+    ?>
+  </div>
 </div>
 
 <?php
