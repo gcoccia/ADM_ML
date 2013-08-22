1c1b7d34939324ff60c0e377e2954a45a34456a1
jdherman <jdherman8@gmail.com>
2013-08-22 13:57:50 -0400

    delete files that arent being used

--- ADM_ML/jsscripts/popupcss.js
+++ ADM_ML/jsscripts/popupcss.js
@@ -1,4 +1,8 @@
-
+function toggle(div_id) {
+	var el = document.getElementById(div_id);
+	if ( el.style.display == 'none' ) {	el.style.display = 'block';}
+	else {el.style.display = 'none';}
+}
 function blanket_size(popUpDivVar) {
 	if (typeof window.innerWidth != 'undefined') {
 		viewportheight = window.innerHeight;
