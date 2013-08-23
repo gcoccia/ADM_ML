03199073cb451984d65a5b56116f4c047afa8174
jdherman <jdherman8@gmail.com>
2013-08-23 10:22:39 -0400

    try strcmp instead

--- sidebar.php
+++ sidebar.php
@@ -48,7 +48,7 @@
     <?php foreach($group->variable as $var) { ?>
       <input id="<?php echo $var['dataset']."_".$var['name'] ?>" 
              type="radio" class="data-radio" name="group1" value="<?php echo $var["name"]?>"
-             <?php if(strcmp($xmlobj->variables->default["tag"],$var["name"] == 0) echo "checked=true"?>> <?php echo $_("".$var["title"])?> <br/>
+             <?php if(strcmp($xmlobj->variables->default["tag"],$var["name"]) == 0) echo "checked=true"?>> <?php echo $_("".$var["title"])?> <br/>
     <?php } ?>
   </div>
 </div>
