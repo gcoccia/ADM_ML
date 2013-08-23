bbbfd6745baa9b491bb1121c5de8723c49d3e4d8
jdherman <jdherman8@gmail.com>
2013-08-23 14:31:30 -0400

    move other timestep-related functions to timestep.js

--- jsscripts/timestep.js
+++ jsscripts/timestep.js
@@ -50,3 +50,68 @@ function update_timestep()
   }
 
 }
+
+function Update_TimeStamp_MP(flag_arrow,flag_timestamp)
+{
+
+  var newtimestamp = new Array(3);
+  var date_temp, i_or_f;
+
+  var initial_date = new Date(parseInt($("#year_initial").val()),
+                           parseInt($("#month_initial").val())-1,
+                           parseInt($("#day_initial").val()));
+  var final_date = new Date(parseInt($("#year_final").val()),
+                           parseInt($("#month_final").val())-1,
+                           parseInt($("#day_final").val()));
+
+  if (flag_timestamp == 0) {
+    date_temp = initial_date;
+    i_or_f = "initial";
+  }
+  else {
+    date_temp = final_date;
+    i_or_f = "final";
+  }
+
+  //Find the next or previous timestamp
+  if (flag_arrow == 1)
+  {
+    date_temp.setDate(date_temp.getDate() + 1);
+    if (flag_timestamp == 0 && date_temp.valueOf() > final_date.valueOf()) return;
+
+    newtimestamp = [date_temp.getFullYear(), date_temp.getMonth() + 1, date_temp.getDate()];
+  }
+  else 
+  {
+    date_temp.setDate(date_temp.getDate() - 1);
+    if (flag_timestamp == 1 && date_temp.valueOf() < initial_date.valueOf()) return;
+
+    newtimestamp = [date_temp.getFullYear(), date_temp.getMonth() + 1, date_temp.getDate()];
+  }
+
+  // Update the time string
+  $("#year_" + i_or_f).val(newtimestamp[0]);
+  $("#month_" + i_or_f).val(newtimestamp[1]);
+  $("#day_" + i_or_f).val(newtimestamp[2]);
+}
+
+function UpdatePopUpTimestep(j)
+{
+  var j;
+  if(j == 0)//Daily time step
+  {
+    document.getElementById('time_interval_text').innerHTML = '({$_("dd/mm/yyyy")})';
+    document.getElementById('gauge_initial_time').innerHTML = '{$_("Initial Time")}:  <input type="text" size=1 name="gauge_day_initial" value=' + gauge_day_initial + '><input type="text" size=1 name="gauge_month_initial" value=' + gauge_month_initial + '><input type="text" size=3 name="gauge_year_initial" value=' + gauge_year_initial + '>';
+    document.getElementById('gauge_final_time').innerHTML = '{$_("Final Time")}:  <input type="text" size=1 name="gauge_day_final" value=' + gauge_day_final + '><input type="text" size=1 name="gauge_month_final" value=' + gauge_month_final + '><input type="text" size=3 name="gauge_year_final" value=' + gauge_year_final + '>';
+    timestep_flag = 1;
+    SwapGaugeImage(image_type);
+  }
+  if(j == 1) //Monthly time step
+  {
+    document.getElementById('time_interval_text').innerHTML = '({$_("mm/yyyy")})';
+    document.getElementById('gauge_initial_time').innerHTML = '{$_("Initial Time")}:  <input type="text" size=1 name="gauge_month_initial" value=' + gauge_month_initial_monthly + '><input type="text" size=3 name="gauge_year_initial" value=' + gauge_year_initial_monthly + '>';
+    document.getElementById('gauge_final_time').innerHTML = '{$_("Final Time")}:  <input type="text" size=1 name="gauge_month_final" value=' + gauge_month_final_monthly + '><input type="text" size=3 name="gauge_year_final" value=' + gauge_year_final_monthly + '>';
+    timestep_flag = 2;
+    SwapGaugeImage(image_type);
+  }
+}
