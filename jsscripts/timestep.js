4ab7a472c56fce352caf0422266fa1397998a063
jdherman <jdherman8@gmail.com>
2013-08-24 21:01:25 -0400

    refactor timestep.js function for conciseness

--- jsscripts/timestep.js
+++ jsscripts/timestep.js
@@ -1,55 +1,35 @@
 function update_timestep()
 {
   var dataset = $("input[name='group1']:checked").attr('id');
-  var current_timestep = $("input[name='ts-radio']:checked").attr('id');
+  var current_timestep = $("input[name='ts-radio']:checked");
   var all_unchecked = true;
 
-  // Check in the XML settings which of these actually exist for this dataset.
-  // If they don't exist, disable the button. Also if it was checked before, check something else instead.
-  if(data_timesteps[dataset].indexOf("D") == -1) {
-    $("input[id='daily']:radio").prop({disabled: true, checked: false});
-    if(""+current_timestep == "daily")
-      $("input[id='monthly']:radio").prop({checked: true});
-  } else {
-    $("input[id='daily']:radio").prop({disabled: false});
-  }
-  current_timestep = $("input[name='ts-radio']:checked").attr('id');
+  // Check which timesteps are disabled in the dataset
+  var DMYdisabled = {"daily": (data_timesteps[dataset].indexOf("D") == -1),
+                     "monthly": (data_timesteps[dataset].indexOf("M") == -1),
+                     "yearly": (data_timesteps[dataset].indexOf("Y") == -1)};
 
-  if(data_timesteps[dataset].indexOf("M") == -1) {
-    $("input[id='monthly']:radio").prop({disabled: true, checked: false});
-    if(""+current_timestep == "monthly")
-      $("input[id='yearly']:radio").prop({checked: true});
-  } else {
-    $("input[id='monthly']:radio").prop({disabled: false});
+  // Disable their respective buttons accordingly
+  for(var dmy in DMYdisabled) {
+    $("input[id='" + dmy + "']:radio").prop('disabled', DMYdisabled[dmy]);
   }
-  current_timestep = $("input[name='ts-radio']:checked").attr('id');
 
-  if(data_timesteps[dataset].indexOf("Y") == -1) {
-    $("input[id='yearly']:radio").prop({disabled: true, checked: false});
-    if(""+current_timestep == "yearly") {
-      if(data_timesteps[dataset].indexOf("D") != -1)
-        $("input[id='daily']:radio").prop({checked: true});
-      else if(data_timesteps[dataset].indexOf("M") != -1)
-        $("input[id='monthly']:radio").prop({checked: true});
+  // If the currently-checked button is now disabled, pick a different one
+  if(current_timestep.prop('disabled')) {
+    current_timestep.prop('checked', false);
+
+    for(var dmy in DMYdisabled) {
+      if(!DMYdisabled[dmy]) {
+        $("input[id='" + dmy + "']:radio").prop('checked', true);
+        break;
+      }
     }
-  }else {
-    $("input[id='yearly']:radio").prop({disabled: false});
   }
+
+  // Disable/Enable the relevant timestamp input boxes depending which radio button is selected
   current_timestep = $("input[name='ts-radio']:checked").attr('id');
-
-  // Disable the timestamp input boxes depending on which timestep is selected
-  if(""+current_timestep == "daily") {
-    $("input[id='day_initial'], input[id='month_initial'], input[id='year_initial']").prop({disabled: false});
-    $("input[id='day_final'], input[id='month_final'], input[id='year_final']").prop({disabled: false});
-  }
-  else if(""+current_timestep == "monthly") {
-    $("input[id='month_initial'], input[id='year_initial'], input[id='month_final'], input[id='year_final']").prop({disabled: false});
-    $("input[id='day_initial'], input[id='day_final']").prop({disabled: true});
-  } else {
-    $("input[id='year_initial'], input[id='year_final']").prop({disabled: false});
-    $("input[id='day_initial'], input[id='month_initial'], input[id='day_final'], input[id='month_final']").prop({disabled: true});
-  }
-
+  $("input[id='day_initial'], input[id='day_final']").prop('disabled', !(""+current_timestep == "daily"));
+  $("input[id='month_initial'], input[id='month_final']").prop('disabled', (""+current_timestep == "yearly"));
 }
 
 function Update_TimeStamp_MP(increment, flag_timestamp)
