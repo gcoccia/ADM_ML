968d8c6fa8fd6e3bda658f56fe713c96b5f3177a
jdherman <jdherman8@gmail.com>
2013-08-25 10:43:29 -0400

    indendation

--- jsscripts/AnimationPrep.js
+++ jsscripts/AnimationPrep.js
@@ -1,71 +1,71 @@
 function ReadTimeInterval()
-	{
-	year_initial = parseInt(document.forms["AnimationForm"]["year_initial"].value);
-	year_final = parseInt(document.forms["AnimationForm"]["year_final"].value);
-	month_initial = parseInt(document.forms["AnimationForm"]["month_initial"].value);
-	month_final = parseInt(document.forms["AnimationForm"]["month_final"].value);
-	day_initial = parseInt(document.forms["AnimationForm"]["day_initial"].value);
-	day_final = parseInt(document.forms["AnimationForm"]["day_final"].value);
-	//Set Time delay between images
-	frames_per_second = parseInt(document.forms["AnimationForm"]["frames_per_second"].value);
-	}
+  {
+  year_initial = parseInt(document.forms["AnimationForm"]["year_initial"].value);
+  year_final = parseInt(document.forms["AnimationForm"]["year_final"].value);
+  month_initial = parseInt(document.forms["AnimationForm"]["month_initial"].value);
+  month_final = parseInt(document.forms["AnimationForm"]["month_final"].value);
+  day_initial = parseInt(document.forms["AnimationForm"]["day_initial"].value);
+  day_final = parseInt(document.forms["AnimationForm"]["day_final"].value);
+  //Set Time delay between images
+  frames_per_second = parseInt(document.forms["AnimationForm"]["frames_per_second"].value);
+  }
 
 function ImageArrayPrep(ImageStrArray,ImageStrRoot,ImageTimeArray)
-	{
-	var day;
-	var month;
-	var mi;
-	var mf;	
-	var di;
-	var df;
-	var Data_Dir = "Data/ADM_Data";
-	var Time_Period;
-	//Because we are lacking SMOS data, we will set the latest date to equate to 5/26/2012
-	if (variable_image_number == 16){
-		//Make the dates
-		SMOS_date_final = new Date(year_final,month_final-1,day_final);
-		SMOS_date_initial = new Date(year_initial,month_initial-1,day_initial);
-		SMOS_date_final_available = new Date(2012,5-1,26);
-		if (SMOS_date_final.getTime() > SMOS_date_final_available.getTime()){
-			year_final = 2012;
-			month_final = 5;
-			day_final = 26;
-			alert("SMOS data is available up to May 26th,2012");
-			}
+  {
+  var day;
+  var month;
+  var mi;
+  var mf; 
+  var di;
+  var df;
+  var Data_Dir = "Data/ADM_Data";
+  var Time_Period;
+  //Because we are lacking SMOS data, we will set the latest date to equate to 5/26/2012
+  if (variable_image_number == 16){
+    //Make the dates
+    SMOS_date_final = new Date(year_final,month_final-1,day_final);
+    SMOS_date_initial = new Date(year_initial,month_initial-1,day_initial);
+    SMOS_date_final_available = new Date(2012,5-1,26);
+    if (SMOS_date_final.getTime() > SMOS_date_final_available.getTime()){
+      year_final = 2012;
+      month_final = 5;
+      day_final = 26;
+      alert("SMOS data is available up to May 26th,2012");
+      }
                 if (SMOS_date_initial.getTime() > SMOS_date_final_available.getTime()){
                         year_initial = 2012;
                         month_initial = 5;
                         day_initial = 26;
                         }
-		}
-	var ndays = [31,28,31,30,31,30,31,31,30,31,30,31];
-	daycount = 0;
-	var t;
-	for (year = year_initial; year < year_final + 1; year++)
-		{
-		if (year == year_initial){mi = month_initial;}
-		else {mi = 1;}
-		if (year == year_final){mf = month_final;}
-		else {mf = 12;}
-		for (month = mi; month < mf + 1; month++)
-			{
-			if (year == year_initial && month == month_initial){di = day_initial}
-			else {di = 1}
-			if (year == year_final && month == month_final){df = day_final}
-			else {df = ndays[month-1]}
-			for (day = di; day < df + 1; day++)
-				{
-			        if (year <= 2008){Time_Period = "/Historical";}
-       				else if (year < 2011){Time_Period = "/Catch_up";}
-       				else if (year == 2011 && month <= 9){Time_Period = "/Catch_up";}
-       				else {Time_Period = "/Realtime";}
-				ImageStrArray[daycount] = Data_Dir + Time_Period + ImageStrRoot + sprintf("%02d",parseInt(year)) + sprintf("%02d",parseInt(month)) + sprintf("%02d",parseInt(day)) + ".gif";
-				ImageTimeArray[daycount] = sprintf("%02d",parseInt(day)) + "/" + sprintf("%02d",parseInt(month)) + "/" + sprintf("%02d",parseInt(year));
-				daycount = daycount + 1
-				}
-			}
-		}
-	}
+    }
+  var ndays = [31,28,31,30,31,30,31,31,30,31,30,31];
+  daycount = 0;
+  var t;
+  for (year = year_initial; year < year_final + 1; year++)
+    {
+    if (year == year_initial){mi = month_initial;}
+    else {mi = 1;}
+    if (year == year_final){mf = month_final;}
+    else {mf = 12;}
+    for (month = mi; month < mf + 1; month++)
+      {
+      if (year == year_initial && month == month_initial){di = day_initial}
+      else {di = 1}
+      if (year == year_final && month == month_final){df = day_final}
+      else {df = ndays[month-1]}
+      for (day = di; day < df + 1; day++)
+        {
+              if (year <= 2008){Time_Period = "/Historical";}
+              else if (year < 2011){Time_Period = "/Catch_up";}
+              else if (year == 2011 && month <= 9){Time_Period = "/Catch_up";}
+              else {Time_Period = "/Realtime";}
+        ImageStrArray[daycount] = Data_Dir + Time_Period + ImageStrRoot + sprintf("%02d",parseInt(year)) + sprintf("%02d",parseInt(month)) + sprintf("%02d",parseInt(day)) + ".gif";
+        ImageTimeArray[daycount] = sprintf("%02d",parseInt(day)) + "/" + sprintf("%02d",parseInt(month)) + "/" + sprintf("%02d",parseInt(year));
+        daycount = daycount + 1
+        }
+      }
+    }
+  }
 
 function ImageArrayPrep_SPI(ImageStrArray,ImageStrRoot,ImageTimeArray)
         {
@@ -79,17 +79,17 @@ function ImageArrayPrep_SPI(ImageStrArray,ImageStrRoot,ImageTimeArray)
         var Time_Period;
         var ndays = [31,28,31,30,31,30,31,31,30,31,30,31];
         var temp;
-	var min;
-	var min_pos;
+  var min;
+  var min_pos;
         daycount = 0;
         var t;
-	var nweeks;
-	var date_temp;
-	var date_initial;
-	//Assume every year is a leap year (2008)
+  var nweeks;
+  var date_temp;
+  var date_initial;
+  //Assume every year is a leap year (2008)
         for (year = year_initial; year < year_final + 1; year++)
                 {
-		date_initial = new Date(2012,0,1);
+    date_initial = new Date(2012,0,1);
                 if (year == year_initial){mi = month_initial;}
                 else {mi = 1;}
                 if (year == year_final){mf = month_final;}
@@ -102,59 +102,59 @@ function ImageArrayPrep_SPI(ImageStrArray,ImageStrRoot,ImageTimeArray)
                         else {df = ndays[month-1]}
                         for (day = di; day < df + 1; day++)
                                 {
-                               	Time_Period = "/Realtime";
-				if (year >= 2012){
-					//Update every week
-					//Find the julian time stamp
-					date_temp = new Date(year,month-1,day);
-					//Calculate the number of days between the time stamp and Jan. 1st
-					temp = Math.floor((date_temp.getTime() - date_initial.getTime())/1000/60/60/24);
-					//Calculate the number of weeks that have been completed
-					nweeks = Math.floor(temp/7);
-					//Calculate the last day for which a week has completed
-					temp = 7*nweeks;
-					//Add these days to Jan 1st to obtain the date
-					date_temp = date_initial;
-					date_temp.setDate(date_temp.getDate() + temp);
-                                	ImageStrArray[daycount] = Data_Dir + Time_Period + ImageStrRoot + sprintf("%02d",parseInt(year)) + sprintf("%02d",parseInt(date_temp.getMonth()+1)) + sprintf("%02d",parseInt(date_temp.getDate())) + ".gif";	
-				}
-				else {
-					//Set to Jan. 1st
-                                	ImageStrArray[daycount] = Data_Dir + Time_Period + ImageStrRoot + sprintf("%02d",parseInt(year)) + sprintf("%02d",parseInt(month)) + sprintf("%02d",parseInt(1)) + ".gif";	
-				}					
+                                Time_Period = "/Realtime";
+        if (year >= 2012){
+          //Update every week
+          //Find the julian time stamp
+          date_temp = new Date(year,month-1,day);
+          //Calculate the number of days between the time stamp and Jan. 1st
+          temp = Math.floor((date_temp.getTime() - date_initial.getTime())/1000/60/60/24);
+          //Calculate the number of weeks that have been completed
+          nweeks = Math.floor(temp/7);
+          //Calculate the last day for which a week has completed
+          temp = 7*nweeks;
+          //Add these days to Jan 1st to obtain the date
+          date_temp = date_initial;
+          date_temp.setDate(date_temp.getDate() + temp);
+                                  ImageStrArray[daycount] = Data_Dir + Time_Period + ImageStrRoot + sprintf("%02d",parseInt(year)) + sprintf("%02d",parseInt(date_temp.getMonth()+1)) + sprintf("%02d",parseInt(date_temp.getDate())) + ".gif"; 
+        }
+        else {
+          //Set to Jan. 1st
+                                  ImageStrArray[daycount] = Data_Dir + Time_Period + ImageStrRoot + sprintf("%02d",parseInt(year)) + sprintf("%02d",parseInt(month)) + sprintf("%02d",parseInt(1)) + ".gif";  
+        }         
                                 ImageTimeArray[daycount] = sprintf("%02d",parseInt(day)) + "/" + sprintf("%02d",parseInt(month)) + "/" + sprintf("%02d",parseInt(year));
                                 daycount = daycount + 1
-				}
-			}
-		}
-	}
+        }
+      }
+    }
+  }
 
 function ChangeTimeStamp(flag_time,i,j)
-	{
-	var flag_time;
-	var j;
-	var i;
-	obj = document.getElementById("TimeStamp").style;
-	//Option 1: Add time stamp to map
-	if (flag_time == 1)
-		{
-		obj.visibility = "visible";
-		obj.height = "100";
-		contentString = "<h2>" + ImageTimeArray[j][i] + "</h2>";
-		//contentString = "<div>" + ImageTimeArray[j][i] + "</div>";
-		document.getElementById('TimeStamp').innerHTML = contentString;
-		}
-	//Option 2: Update time stamp on map
-	if (flag_time == 2)
-		{
-		contentString = "<h2>" + ImageTimeArray[j][i] + "</h2>";;
-		//contentString = "<div>" + ImageTimeArray[j][i] + "</div>";
-		document.getElementById('TimeStamp').innerHTML = contentString;
-		}
-	//Option 3: Remove time stamp form map
-	if (flag_time == 3)
-		{
-		obj.visibility = "hidden";
-		obj.height = "";
-		}
-	}
+  {
+  var flag_time;
+  var j;
+  var i;
+  obj = document.getElementById("TimeStamp").style;
+  //Option 1: Add time stamp to map
+  if (flag_time == 1)
+    {
+    obj.visibility = "visible";
+    obj.height = "100";
+    contentString = "<h2>" + ImageTimeArray[j][i] + "</h2>";
+    //contentString = "<div>" + ImageTimeArray[j][i] + "</div>";
+    document.getElementById('TimeStamp').innerHTML = contentString;
+    }
+  //Option 2: Update time stamp on map
+  if (flag_time == 2)
+    {
+    contentString = "<h2>" + ImageTimeArray[j][i] + "</h2>";;
+    //contentString = "<div>" + ImageTimeArray[j][i] + "</div>";
+    document.getElementById('TimeStamp').innerHTML = contentString;
+    }
+  //Option 3: Remove time stamp form map
+  if (flag_time == 3)
+    {
+    obj.visibility = "hidden";
+    obj.height = "";
+    }
+  }
