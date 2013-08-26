c70680222878c15aa4a60ba7cd16d970e36053b7
jdherman <jdherman8@gmail.com>
2013-08-26 17:09:17 -0400

    title font

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ $info_box_strings = array(1 => $_("Weather data used to drive the hydrologic mod
 <meta http-equiv="content-type" content="text/html; charset=UTF-8"/> 
 <link rel="stylesheet" href="css/s.css" /> 
 <link rel="stylesheet" type="text/css" media="screen,projection" href="css/Moz.css" title="Moz" />
-<link href='http://fonts.googleapis.com/css?family=Raleway:400,200,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:200' rel='stylesheet' type='text/css'>
 <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript" src="jsscripts/popupcss.js"></script>
