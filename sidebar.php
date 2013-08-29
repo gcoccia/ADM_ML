0659ec040fa6fa0c575d514f24e670bde5d943e7
jdherman <jdherman8@gmail.com>
2013-08-29 18:24:18 -0400

    trying nav pills:

--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,9 @@
 <ul class="nav nav-list" data-spy="affix" data-offset-top="900">
 <li class="nav-header" style="background: linear-gradient(rgb(238, 238, 238), rgb(204, 204, 204)); border-radius: 5px 5px 0px 0px;"><?php echo $_("Data Extraction") ?></li>
-<div class="btn-group form-inline">
-  <label class="radio inline">
-           <input id="none" type="radio" class="de-radio" name="de-radio" checked="true" onclick='Update_Listeners("none")'> None
-        </label>
-        <label class="radio inline">                 
-           <input id="point" type="radio" class="de-radio" name="de-radio" onclick='Update_Listeners("point")'>Point
-        </label>
-        <label class="radio inline">
-           <input id="spatial" type="radio" class="de-radio" name="de-radio" onclick='Update_Listeners("spatial")'>Spatial
-  </label>
-</div>
-
+<ul class="nav nav-pills">
+<li class="active"><a href="javascript:void(0)" id="none" class="de-radio" onclick='Update_Listeners("none")'>None</a></li>
+<li><a href="javascript:void(0)" id="point" class="de-radio" onclick='Update_Listeners("point")'>Point</a></li>
+<li><a href="javascript:void(0)" id="spatial" class="de-radio" onclick='Update_Listeners("Spatial")'>Spatial</a></li>
          
         <li class="nav-header" style="background: linear-gradient(rgb(238, 238, 238), rgb(204, 204, 204)); border-radius: 5px 5px 0px 0px;"><?php echo $_("Animation Interface")?></li> 
       <form id="AnimationForm" name="AnimationForm">
