7ce644c27eadc1a6db1f751b5ec4c84c319d012f
jdherman <jdherman8@gmail.com>
2013-08-29 15:44:20 -0400

    forget the extra file, it was working before

--- jsscripts/data_extraction.js
+++ jsscripts/data_extraction.js
@@ -1,24 +1,84 @@
+var mapPolygon = null,
+    followLine1 = null,
+    followLine2 = null;
+var polyOptions = { map : map_array[0],
+                    strokeColor   : '#ff0000',
+                    strokeOpacity : 0.6,
+                    strokeWeight  : 4,
+                    path:[]
+                  };
+var lineOptions = { clickable: false,
+                    map : map_array[0],
+                    path: [],
+                    strokeColor: "#787878",
+                    strokeOpacity: 1,
+                    strokeWeight: 2
+                  };
+
 function Update_Listeners(type){
 
-  if (type == 'none'){
-    finalizePolygon();
-    removePolygon();
+ if (type == 'none'){
+  //Remove the listeners and lines/polygons from the map
+  if(mapPolygon) {
+    mapPolygon.stopEdit();
+    mapPolygon.setMap(null);
+    google.maps.event.clearListeners(mapPolygon, "click");
   }
-  else if (type == 'point'){
-    //Remove present listeners
-    Update_Listeners('none')
-    //Add the listeners
-    google.maps.event.addListener(map_array[0], 'click', function(mEvent) {Point_Data(mEvent.latLng)});
-  }
-  else if (type == 'spatial'){
-    //Remove present listeners
-    Update_Listeners('none');
-    newPolygon();
+  if(followLine1) followLine1.setMap(null);
+  if(followLine2) followLine2.setMap(null);
+  google.maps.event.clearListeners(map_array[0], "click");
+  google.maps.event.clearListeners(map_array[0], "mousemove");
+  google.maps.event.clearListeners(map_array[0], "rightclick");
+  map_array[0].setOptions({draggableCursor:null});
+ }
+ else if (type == 'point'){
+  //Remove present listeners
+  Update_Listeners('none')
+  //Add the listeners
+  google.maps.event.addListener(map_array[0], 'click', function(mEvent) {Point_Data(mEvent.latLng)});
+ }
+ else if (type == 'spatial'){
+  //Remove present listeners
+  Update_Listeners('none');
+  map_array[0].setOptions({draggableCursor:'crosshair'});
+  // Add polygon and lines to map
+  mapPolygon = new google.maps.Polygon(polyOptions);
+  followLine1 = new google.maps.Polyline(lineOptions);
+  followLine2 = new google.maps.Polyline(lineOptions);
 
-    google.maps.event.addListener(mapPolygon, 'click', function() {
-      Spatial_Data();
-    });
-  }
+  // Add event handlers related to polygon drawing
+  google.maps.event.addListener(map_array[0], 'click', function(point) {
+       mapPolygon.stopEdit();
+       mapPolygon.getPath().push(point.latLng);
+       mapPolygon.runEdit(true);
+  });
+     
+  google.maps.event.addListener(map_array[0], 'rightclick', function () {
+    followLine1.setMap(null);
+    followLine2.setMap(null);
+    google.maps.event.clearListeners(map_array[0], "click");
+    google.maps.event.clearListeners(map_array[0], "mousemove");
+    google.maps.event.clearListeners(map_array[0], "rightclick");
+    map_array[0].setOptions({draggableCursor:null});
+  });
+     
+  google.maps.event.addListener(map_array[0], 'mousemove', function(point) {
+    var pathLength = mapPolygon.getPath().getLength();
+    if (pathLength >= 1) {
+      var startingPoint1 = mapPolygon.getPath().getAt(pathLength - 1);
+      var followCoordinates1 = [startingPoint1, point.latLng];
+      followLine1.setPath(followCoordinates1);
+      var startingPoint2 = mapPolygon.getPath().getAt(0);
+      var followCoordinates2 = [startingPoint2, point.latLng];
+      followLine2.setPath(followCoordinates2);
+    }
+  });
+  
+  google.maps.event.addListener(mapPolygon, 'click', function() {
+    Spatial_Data();
+  });
+
+ }
 }
 
 function Point_Data(latLng){
