87d647d3c3e12b4cc8d47418ec7be8f4cc02c0c1
jdherman <jdherman8@gmail.com>
2013-08-30 09:32:11 -0400

    need more php debug for xml

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
 if (file_exists('settings.xml')) {
-  $xmlobj = simplexml_load_file("settings.xml"); echo $xmlobj;
+  $xmlobj = simplexml_load_file("settings.xml");
+  if ($xmlobj === false) {
+    echo "Failed loading XML\n";
+    foreach(libxml_get_errors() as $error) {
+        echo "\t", $error->message;
+    }
+  }
 } else { exit("Error: settings.xml file not found."); }
 
 require_once('php-gettext-1.0.11/gettext.inc');
