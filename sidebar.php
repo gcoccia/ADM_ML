7ee4b5f60504f990505db6a36bd068da1e6039db
jdherman <jdherman8@gmail.com>
2013-08-30 08:58:43 -0400

    convert dmy to pills

--- sidebar.php
+++ sidebar.php
@@ -1,50 +1,42 @@
 <ul class="nav nav-list" data-spy="affix" data-offset-top="900">
-<ul class="nav nav-pills data-extraction">
-  <li class="active"><a href="javascript:void(0)" id="none" class="de-box" onclick='Update_Listeners("none")'><?php echo $_("Animation")?></a></li>
-  <li><a href="javascript:void(0)" id="point" class="de-box" onclick='Update_Listeners("point")'><?php echo $_("Point Data")?></a></li>
-  <li><a href="javascript:void(0)" id="spatial" class="de-box" onclick='Update_Listeners("spatial")'><?php echo $_("Spatial Data")?></a></li>
-</ul>
-   
-<li class="divider"></li>
+  <ul class="nav nav-pills data-extraction">
+    <li class="active"><a href="javascript:void(0)" id="none" class="de-pills" onclick='Update_Listeners("none")'><?php echo $_("Animation")?></a></li>
+    <li><a href="javascript:void(0)" id="point" class="de-pills" onclick='Update_Listeners("point")'><?php echo $_("Point Data")?></a></li>
+    <li><a href="javascript:void(0)" id="spatial" class="de-pills" onclick='Update_Listeners("spatial")'><?php echo $_("Spatial Data")?></a></li>
+  </ul>
 
-        <li class="nav-header" style="background: linear-gradient(rgb(238, 238, 238), rgb(204, 204, 204)); border-radius: 5px 5px 0px 0px;"><?php echo $_("Animation Interface")?></li> 
-      <form id="AnimationForm" name="AnimationForm">
-         <div class="btn-group form-inline">
-	   <label class="control-label"><?php echo $_("Timestep").":" ?></label>
-	      <label class="radio inline">
-                 <input id="daily" type="radio" name="ts-radio" checked="true"> Daily 
-              </label>
-	      <label class="radio inline">
-                 <input id="monthly" type="radio" name="ts-radio"> Monthly 
-              </label>
-              <label class="radio inline">
-                 <input id="yearly" type="radio" name="ts-radio"> Yearly
-              </label>
-         </div>
-         <label class="control-label"><?php echo $_("Time Interval")." (".$_("dd/mm/yyyy")."):"?></label>
-        <div class="control-group form-inline">
-          <label><?php echo $_("Initial")?>:</label>
-      	    <input id="day_initial" type="text" maxlength="2" name="day_initial" value=<?php echo $day_initial ?> style="width:15px;">
-            <input id="month_initial" type="text" maxlength="2" name="month_initial" value=<?php echo $month_initial ?> style="width:15px;">
-            <input id="year_initial" type="text" maxlength="4" name="year_initial" value=<?php echo $year_initial ?> style="width:35px;">
-      	    <input type="button" value="-" class="btn btn-mini" onclick="Update_TimeStamp_MP(-1,0)">
-            <input type="button" value="+" class="btn btn-mini" onclick="Update_TimeStamp_MP(1,0)">
-         </div>
-         <div class="control-group form-inline">
-            <label><?php echo $_("Final") ?>:</label>
-            <input id="day_final" type="text" maxlength="2" name="day_final" value=<?php echo $day_final ?> style="width:15px;">
-            <input id="month_final" type="text" maxlength="2" name="month_final" value=<?php echo $month_final ?> style="width:15px;">
-            <input id="year_final" type="text" maxlength="4" name="year_final" value=<?php echo $year_final ?> style="width:35px;">
-            <input type="button" value="-" class="btn btn-mini" onclick="Update_TimeStamp_MP(-1,1)">
-            <input type="button" value="+" class="btn btn-mini" onclick="Update_TimeStamp_MP(1,1)"><br/>
-         </div>
+  <li class="divider"></li>
+  <li class="nav-header"><?php echo $_("Select Date Range")?></li>
 
-      <table><tr>
-      <td><input id="update_interval" type="button" class="btn" value=<?php echo $_("Update time interval")?> ></td>
-      <td><input id="clear_all" type="button" class="btn" value=<?php echo $_("Clear all overlays")?> ></td>
-      </tr>
-      </table>
-     </form>
+  <form id="AnimationForm" name="AnimationForm">
+    <ul class="nav nav-pills data-extraction">
+      <li class="active"><a href="javascript:void(0)" id="daily" class="ts-pills"><?php echo $_("Daily")?></a></li>
+      <li><a href="javascript:void(0)" id="monthly" class="ts-pills"><?php echo $_("Monthly")?></a></li>
+      <li><a href="javascript:void(0)" id="yearly" class="ts-pills"><?php echo $_("Yearly")?></a></li>
+    </ul>
+    <label class="control-label"><?php echo $_("Time Interval")." (".$_("dd/mm/yyyy")."):"?></label>
+    <div class="control-group form-inline">
+      <label><?php echo $_("Initial")?>:</label>
+	    <input id="day_initial" type="text" maxlength="2" name="day_initial" value=<?php echo $day_initial ?> style="width:15px;">
+      <input id="month_initial" type="text" maxlength="2" name="month_initial" value=<?php echo $month_initial ?> style="width:15px;">
+      <input id="year_initial" type="text" maxlength="4" name="year_initial" value=<?php echo $year_initial ?> style="width:35px;">
+	    <input type="button" value="-" class="btn btn-mini" onclick="Update_TimeStamp_MP(-1,0)">
+      <input type="button" value="+" class="btn btn-mini" onclick="Update_TimeStamp_MP(1,0)">
+    </div>
+    <div class="control-group form-inline">
+      <label><?php echo $_("Final") ?>:</label>
+      <input id="day_final" type="text" maxlength="2" name="day_final" value=<?php echo $day_final ?> style="width:15px;">
+      <input id="month_final" type="text" maxlength="2" name="month_final" value=<?php echo $month_final ?> style="width:15px;">
+      <input id="year_final" type="text" maxlength="4" name="year_final" value=<?php echo $year_final ?> style="width:35px;">
+      <input type="button" value="-" class="btn btn-mini" onclick="Update_TimeStamp_MP(-1,1)">
+      <input type="button" value="+" class="btn btn-mini" onclick="Update_TimeStamp_MP(1,1)"><br/>
+    </div>
+  <table><tr>
+  <td><input id="update_interval" type="button" class="btn" value=<?php echo $_("Update time interval")?> ></td>
+  <td><input id="clear_all" type="button" class="btn" value=<?php echo $_("Clear all overlays")?> ></td>
+  </tr>
+  </table>
+  </form>
 
      <!--       Consider floating the opacity options over the map <?php echo $_("Image Opacity")?>:
       <input type="button" value="-" onclick="update_overlay_opacity(0)">
