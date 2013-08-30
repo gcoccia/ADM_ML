b9c458e4e0c39d27298968999295671cc0bb5345
jdherman <jdherman8@gmail.com>
2013-08-30 15:27:24 -0400

    sidebar spatial dropdowns

--- sidebar.php
+++ sidebar.php
@@ -127,12 +127,14 @@
                   <?php echo $datatype['title'] ?>
                   <b class="caret"></b>
                 </a>
-                <!-- <div class="dropdown-menu checkbox inline">
+                <ul class="dropdown-menu checkbox inline">
                   <li class="nav-header"><?php echo $_("Dataset")?></li>
                   <?php foreach($datatype->dataset as $dataset) { ?>
-                  <label><input type="checkbox" name="variables_spatial_data[]" value="<?php echo $datatype['name']."-".$dataset['name'] ?>" href="javascript:void(0)"><i></i><?php echo $dataset['name']?></label>
+                  <li>
+                    <label><input type="checkbox" name="variables_spatial_data[]" value="<?php echo $datatype['name']."-".$dataset['name'] ?>" href="javascript:void(0)"><i></i><?php echo $dataset['name']?></label>
+                  </li>
                   <?php } ?>
-                </div> -->
+                </ul>
               </li>
     <?php   }
           } 
