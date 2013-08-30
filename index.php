633d7afc25449b8488ff20c1658471f37eaba018
jdherman <jdherman8@gmail.com>
2013-08-30 09:48:02 -0400

    interesting typo in including bootstrap.js

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ $info_box_strings = array(1 => $_("Weather data used to drive the hydrologic mod
 <script type="text/javascript" src="jsscripts/data_extraction.js"></script>
 <script src="http://code.highcharts.com/highcharts.js" type="text/javascript"></script>
 <script src="http://code.highcharts.com/modules/exporting.js" type="text/javascript"></script>
-<script type="text/javasdcript" src="jsscripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="jsscripts/bootstrap.min.js"></script>
 <script type="text/javascript">
   var basinImage  = <?php echo $mask_gauge ?>;
   var info_box_strings = <?php echo json_encode($info_box_strings, JSON_NUMERIC_CHECK) ?>;
