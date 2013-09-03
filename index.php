8cb01c945d24f1e1ad38c21c791d393a5accadee
jdherman <jdherman8@gmail.com>
2013-09-03 16:51:24 -0400

    fix data extraction string getting

--- index.php
+++ index.php
@@ -185,7 +185,7 @@ $info_box_strings = array("Meteorology" => $_("Weather data used to drive the hy
      });
 
     $("#update_interval").click(function() {
-      var current_setting = $("ul.de-selection li.active>a").attr('id');
+      var current_setting = $("ul.data-extraction li.active>a").attr('id');
       if(""+current_setting == "none") update_animation();
       else if(""+current_setting == "point") Create_Point_Plot();
       else Update_Spatial_Data_Display();
