0a4ed7e179f0201004bee10d1889beb78472377e
jdherman <jdherman8@gmail.com>
2013-09-10 13:55:23 -0400

    include jquery ui for slider

--- index.php
+++ index.php
@@ -85,6 +85,8 @@ $info_box_strings = array("Meteorology" => $_("Weather data used to drive the hy
 <link rel="stylesheet" type="text/css" href="css/Moz.css" title="Moz">
 <link href='http://fonts.googleapis.com/css?family=Raleway:200' rel='stylesheet' type='text/css'>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <script type="text/javascript" src="jsscripts/bootstrap.min.js"></script>
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript" src="jsscripts/MiscFunctions.js"></script>
