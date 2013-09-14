fb9d18a2a0cf45869d293ae3049839284d34aa29
jdherman <jdherman8@gmail.com>
2013-09-14 10:42:13 -0400

    trying to keep icon

--- interface.php
+++ interface.php
@@ -286,8 +286,7 @@ $info_box_strings = array("Meteorology" => $_("Weather data used to drive the hy
       var copyLi = $(this).parent().clone();
       copyLi.appendTo("ul#currently-selected-vars");
       copyLi.find('a').text(copyLi.find('a').attr('id')); // give new element full ID including datatype
-      copyLi.find('a>i').removeClass('icon-plus-sign');
-      copyLi.find('a>i').prepend("<i class='icon-remove'></i>");
+      copyLi.find('a').prepend("<i class='icon-remove'></i>");
       
       $(this).parent().hide();
       Update_Spatial_Data_Display();
