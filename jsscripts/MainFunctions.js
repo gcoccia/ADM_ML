1eb7835df9e847ee40904fba3b4421922b0941f6
Nathaniel Chaney <chaneyna@gmail.com>
2013-09-19 22:14:48 -0400

    Played with the widths/heights

--- jsscripts/MainFunctions.js
+++ jsscripts/MainFunctions.js
@@ -115,8 +115,8 @@ function update_basic()
 
       var div = document.createElement("div");
       div.style.position = "absolute";
-      div.style.left = "20%";
-      div.style.top = "120px";
+      div.style.left = "10%";
+      div.style.top = "50px";
       div.setAttribute('id','basic_image');
       var img = document.createElement("img");
       img.setAttribute('id',dataset);
