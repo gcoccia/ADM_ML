e0718e36f70385afdde28962b600b459321d5cf9
chaneyn <chaneyna@gmail.com>
2013-10-02 19:58:33 -0400

    Added language scripts

---
+++ languages/scripts/prepare_template.py
@@ -0,0 +1,4 @@
+#Script to prepare the template .pot file
+#1.Read in settings.xml.ref
+#2.Recursively create the pot file
+#3.Copy to the different languages
---
+++ languages/scripts/process_language.py
@@ -0,0 +1,1 @@
+#convert .po files to .mo files for the server to read
