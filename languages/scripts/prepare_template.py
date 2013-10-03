3ac6bab64039bba1d465223b748281df369f7582
chaneyn <chaneyna@gmail.com>
2013-10-02 20:24:21 -0400

    Adjusting how the template is made

--- languages/scripts/prepare_template.py
+++ languages/scripts/prepare_template.py
@@ -1,4 +1,38 @@
-#Script to prepare the template .pot file
-#1.Read in settings.xml.ref
+import xml.etree.ElementTree as ET
+import os
+
+def Read_XML():
+
+ #Open file
+ tree = ET.parse('../../../settings.xml.ref')
+ root = tree.getroot()
+
+ #Pull dataset titles
+ datasets = []
+ groups = root.find('variables').findall('group')
+ for group in groups:
+  group_name = group.attrib['name']
+  for variable in group.findall('datatype'):
+   variable_name = variable.attrib['name']
+   variable_units = variable.attrib['units']
+   variable_mask = variable.attrib['mask']
+   for dataset in variable.findall('dataset'):
+    dataset_title = dataset.attrib['title']
+    if dataset_title not in datasets:
+     datasets.append(dataset_title)
+
+ return datasets
+
+datasets = Read_XML()
+
+#Write ot temporary php file
+fp = open('settings_xml.php','w')
+fp.write('<?php\n')
+for dataset in datasets:
+ fp.write("$_('%s')\n" % dataset)
+fp.write('?>\n')
+fp.close()
+
 #2.Recursively create the pot file
-#3.Copy to the different languages
+os.chdir('../..')
+os.system('xgettext --from-code=utf-8 -k_e -k_x -k__ -o languages/templates/awcm.pot $(find . -name "*.php")')
